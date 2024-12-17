7803abb1385f3f25f3fdb128cb0c1d2a0ae7b957
Racoon AuChocolat <atilanoe.valero98@gmail.com>
2024-12-17 23:54:57 +0000

    modified:   package-lock.json 	modified:   package.json 	modified:   server.js  didi the thing

--- server.js
+++ server.js
@@ -10,13 +10,24 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+const bcrypt = require("bcrypt");
 
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+      console.log(res);
+    });
+  });
+  
 
 
 //END_ASYNC
 
 //START_SYNC
 
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
 
 
 //END_SYNC
